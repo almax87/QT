c53e81b4e3684c4a457a4786bfa919b0d075b76d
almax <amax87@mail.ru>
2025-07-16 14:46:15 +0300

    remove

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_10_TCPclient_FILE C:/Users/almax/Documents/Netology/QT/Lesson10/PreTCPclient/TCPclient/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/10_TCPclient.exe)
-set(__QT_DEPLOY_TARGET_10_TCPclient_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_10_TCPclient_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
